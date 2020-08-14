c3c901456cd20a9f5ecb6ff4e4af77c242e7389e
Archives <archives@archivess-mbp.mynet>
2020-08-14 16:00:19 +0100

    setup changed

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 import setuptools
-from setuptools import find_packages
+from setuptools import dist, find_packages
+dist.Distribution().fetch_build_eggs(['Cython==0.29'])
+from Cython.Build import cythonize
+from setuptools.command.build_py import build_py as build_py_orig
 
-with open("./README.md", "r") as fh:
+
+class CustomBuildPy(build_py_orig):
+    """
+    subclass build_py so that we collect no .py files inside the built pip package
+    this is done by overriding build_packages method with a noop
+    """
+    def build_packages(self):
+        pass
+
+
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('./requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
-install_requires = [req for req in install_requires if req != '' and not req.startswith('#')]
+directives = {
+    'language_level': 3,
+    'always_allow_keywords': True
+}
 
 setuptools.setup(
     name="caching",
@@ -18,12 +31,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MonolithAILtd/caching",
-    packages=find_packages(include="caching.*", exclude=["tests"]),
-    install_requires=install_requires,
+    install_requires=["boto3"],
+    packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta"
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3',
+    tests_require=['pytest'],
+    ext_modules=cythonize("caching/**/*.py", exclude="tests/**/*.py", compiler_directives=directives, nthreads=4),
+    cmdclass={'build_py': CustomBuildPy},
+    include_package_data=False,
+    options={"bdist_wheel": {"universal": "1"}}
 )
